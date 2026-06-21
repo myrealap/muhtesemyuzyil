3ea3b7cf601c189f10d89f2f3d43ba8c7dc83869
myrealap <myrealap@gmail.com>
2026-06-22 02:57:20 +0330

    Add files via upload

---
+++ js/ActiveMenu.js
@@ -0,0 +1,39 @@
+﻿$(function () {
+   
+    SetActive();
+    function GetCurrentPageName() {
+
+        var sPath = window.location.pathname;
+        var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
+
+        return sPage.toLowerCase()
+    }
+
+    function CutLink(deger) {
+
+        var sPath = deger;
+        var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
+
+        return sPage.toLowerCase()
+    }
+
+    function SetActive() {
+        $("#foo0 a").each(function () {
+
+            if (CutLink($(this).attr("href").toLowerCase()) == GetCurrentPageName()) {
+                var myClass = $(this).attr("class");
+                myClass += "Active";
+                $(this).addClass(myClass);
+                
+                
+                if (parseInt($(this).attr("rel")) > 5) {
+
+                    $("#foo0").trigger("slideToPage", 1);
+                }
+            }
+
+        });
+    }
+
+
+});
---
+++ js/PreLoadCustom.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $("#Preimages").animate({ "opacity": "0" }, "fast");
+    setTimeout(function () {
+        $("#PreloadDiv").hide();
+
+
+        $("#Preimages").animate({ "opacity": "1" }, 3000);
+    }, 5800);
+
+});
